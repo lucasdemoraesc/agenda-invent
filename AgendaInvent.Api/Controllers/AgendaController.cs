a60a381247835c4c817cc7b2cef29e9f6d3bb723
Lucas Moraes <48826783+lucasdemoraesc@users.noreply.github.com>
2020-02-16 18:07:43 -0300

    Compressão e cache

--- AgendaInvent.Api/Controllers/AgendaController.cs
+++ AgendaInvent.Api/Controllers/AgendaController.cs
@@ -9,7 +9,7 @@ using System.Web.Http;
 
 namespace AgendaInvent.Api.Controllers
 {
-    [RoutePrefix("api/contacts")]
+    [RoutePrefix("api/agenda")]
     public class AgendaController : ApiController
     {
         private IContactService _service;
